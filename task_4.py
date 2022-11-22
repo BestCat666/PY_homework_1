86bd3ec41ce7708abeb6c22597c4770c29db2856
Best Cat <kotova-irina11@rambler.ru>
2022-11-22 19:29:46 +0300

    Ex4 диапазон координат

--- task_4.py
+++ task_4.py
@@ -1,11 +1,11 @@
 # Задача 4: Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
 print("Введите номер координатной четверти от 1 до 4: ")
-Four_Quadrant  = int(input())
-if Four_Quadrant == 1:
+fourQuadrant  = int(input())
+if fourQuadrant == 1:
     print("x(0 , оо)  y (0 , oo)")
-elif Four_Quadrant == 2:
+elif fourQuadrant == 2:
     print("x(-oo , 0)  y (0 , oo)")
-elif Four_Quadrant == 3:
+elif fourQuadrant == 3:
     print("x(-oo , 0)  y (-oo , 0)")
-elif Four_Quadrant == 4:
+elif fourQuadrant == 4:
     print("x(0 , оо)  y (-oo , 0)") 
